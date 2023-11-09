22144cd0605f8683a5335d0fe73ff30e43b7d27f
Vasilii Iurev <yuryev.v@Vasiliis-Macbook-Pro.local>
2023-11-09 12:35:07 -0500

    add main.py with comments

---
+++ main.py
@@ -0,0 +1,20 @@
+# import publisher, subscriber, dashboard
+
+
+# code for traffic light
+
+
+# code for camera
+
+
+# launch publisher in a thread
+
+
+# launch dashboard in a thread
+
+# launch subsciber in a thread, update dashboard on message
+
+
+# loop with traffic light, constantly checking the api's
+# if conditions are met, trigger publish
+# before publish, take picture and include path in the payload
